d35404bc394e1b145b6b87eab5eb93a37cbdac0f
Damon Schulz <damon.schulz@gmail.com>
2020-11-06 11:01:09 -0600

    added bcrypt module, added hash and salt to users routes.

--- tracker/Server/routes/users.js
+++ tracker/Server/routes/users.js
@@ -1,4 +1,5 @@
 const {User, validate} = require('../models/user');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const router = express.Router();
 
@@ -34,16 +35,19 @@ router.post('/', async(req, res) => {
         const {error} = validate(req.body);
         if (error) 
             return res.status(400).send(error);
+
+        let user = await User.findOne({ email: req.body.email });
+        if (user) return res.status(400).send('User already registered.');
         
+        const salt = await bcrypt.genSalt(10);
         const user = new User({
             firstName: req.body.firstName,
             lastName: req.body.lastName, 
             email: req.body.email,
-            password: req.body.password});
+            password: await bcrypt.hash(req.body.password, salt),});
 
         await user.save();
-        return res.send(user);
-
+        return res.send({ _id: user._id, firstName: user.firstName,lastName: user.lastName, email: user.email })
     } catch (ex) {
         return res
             .status(500)
